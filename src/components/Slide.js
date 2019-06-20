13e097504eec0ca71689fcbc7328e6c76b331246
Piyush Mehta <me@piyushmehta.com>
2019-06-20 23:24:03 +0530

    slider for month

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -3,10 +3,12 @@ import React from "react";
 import { Slider, InputNumber, Row, Col } from "antd";
 
 export default class Slide extends React.Component {
-  state = {
-    inputValue: 1
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: 1
+    };
+  }
   onChange = value => {
     this.setState({
       inputValue: value
@@ -17,19 +19,19 @@ export default class Slide extends React.Component {
     const { inputValue } = this.state;
     return (
       <Row>
-        <Col span={12}>
+        <Col span={8}>
           <Slider
-            min={1}
-            max={20}
+            min={this.props.min}
+            max={this.props.max}
             onChange={this.onChange}
             value={typeof inputValue === "number" ? inputValue : 0}
           />
         </Col>
         <Col span={4}>
           <InputNumber
-            min={1}
-            max={20}
-            style={{ marginLeft: 16 }}
+            min={this.props.min}
+            max={this.props.max}
+            style={{ marginLeft: 2 }}
             value={inputValue}
             onChange={this.onChange}
           />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 import { Layout, Menu, Breadcrumb } from "antd";
 import "./Home.css";
+import { Slider, InputNumber, Row, Col } from "antd";
 const { Header, Content, Footer } = Layout;
 
 export default class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputAmoutValue: 1
+    };
+  }
+  _onAmountChange = value => {
+    this.setState({
+      inputAmountValue: value
+    });
+  };
+  _onMonthChange = value => {
+    this.setState({
+      inputAmountValue: value
+    });
+  };
   render() {
     return (
       <div>
@@ -13,22 +30,35 @@ export default class componentName extends Component {
             <Menu
               theme="dark"
               mode="horizontal"
-              defaultSelectedKeys={["2"]}
+              defaultSelectedKeys={["1"]}
               style={{ lineHeight: "64px" }}
             >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
+              <Menu.Item key="1">Loan Calculator</Menu.Item>
             </Menu>
           </Header>
           <Content style={{ padding: "0 50px" }}>
             <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>Loan Calculator</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-              Content
+            <div style={{ background: "#fff", padding: 34, minHeight: 720 }}>
+              <Row>
+                <Col span={8}>
+                  <label> Loan</label>
+                  <Slider
+                    min={500}
+                    max={5000}
+                    onChange={this._onAmountChange}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col span={8}>
+                  <label>Duration</label>
+                  <Slider min={6} max={24} onChange={this._onMonthChange} />
+                </Col>
+              </Row>
             </div>
           </Content>
           <Footer style={{ textAlign: "center" }}>Piyush Mehta ©2019</Footer>
