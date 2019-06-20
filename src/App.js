8577bf751fb68ca39a45cbf0d17990c9fc0af357
Piyush Mehta <me@piyushmehta.com>
2019-06-20 23:04:27 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "antd/dist/antd.css";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
